1226f2e0a8251e96569631ce9b721b6ce89023fb
junheenim <101177178+junheenim@users.noreply.github.com>
2022-08-09 21:47:04 +0900

    ve

--- Source.c
+++ Source.c
@@ -12,9 +12,22 @@ void main()
 
 	// 대입 연산자 "="는 오른쪽에 있는 피연산자의 값을 왼쪽에 있는 피연산자에게 저장
 
-	/*
+	char alphabet = 'A';
 	int box = 100;
+	float pi = 3.14;
+
+	// 문자열 100
+	print("100\n");
+
+	// box라는 변수에 저장되어 있는 값을 출력하는 100
 	printf("%d\n", box);
+
+
+	printf("%f\n", pi);
+
+
+	printf("%c\n", alphabet);
+
 	printf("안녕하세요? 정성진입니다.");
-	*/
+		   
 }
