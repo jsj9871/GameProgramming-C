e542c01991e3650bd127b3e30fc5ed6c778c03fd
junheenim <101177178+junheenim@users.noreply.github.com>
2022-08-10 21:46:19 +0900

    2022.08.10

---
+++ 2022.08.09.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <stdio.h>
+
+/*
+void main()
+{
+	// 자료형 <- 데이터를 저장하기 위해 데이터의 종류를 알려주는 것
+
+	// 문자, 정수, 실수
+	// char <- 한 문자 저장
+	// int <- 정수 저장
+	// float <- 실수 저장
+
+
+	// 대입 연산자 "="는 오른쪽에 있는 피연산자의 값을 왼쪽에 있는 피연산자에게 저장
+
+	char alphabet = 'A';
+	int box = 100;
+	float pi = 3.14;
+
+	// 문자열 100
+	printf("100\n");
+
+	// box라는 변수에 저장되어 있는 값을 출력하는 100
+	printf("%d\n", box);
+
+
+	printf("%f\n", pi);
+
+
+	printf("%c\n", alphabet);
+
+	printf("안녕하세요? 정성진입니다.");
+
+}
+*/
