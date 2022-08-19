3b78c664c02e6df7221e1bb8cf82638768755769
ksy2949871@naver.com <ksy2949871@naver.com>
2022-08-19 21:47:20 +0900

    2022.08.19(변수의 주소)

---
+++ 2022.08.18(자료형 변환).h
@@ -0,0 +1,103 @@
+#pragma once
+#include <stdio.h>
+
+void main()
+{
+	// 구구단
+	/*
+	for (int i = 2; i <= 9; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			printf("%d x %d = %d\n", i, j, i * j);
+		}
+
+		printf("\n");
+	}
+	*/
+
+	// while문
+	// 특정 조건을 만족할 때까지 계속 주어진 명령문 실행하는 반복문
+	/*
+	int memory = 5;
+
+	while (memory > 1)
+	{
+		memory--;
+		printf("조건이 참입니다.\n");
+	}
+	*/
+
+	// do while 문
+	// 조건과 상관없이 한 번의 작업을 수행한 다음 조건에 따라 명령문 실행하는 반복문
+	/*
+	int value = 5;
+
+	do
+	{	// do에서 선언한 변수는 while의 조건으로 사용 X
+		// int x = 10;
+		printf("조건이 거짓입니다.");
+	}
+	while (value == 10);
+	*/
+
+	// continue 문
+	// 해당 조건문만 실행하지 않고, 반복문은 이어서 실행하는 제어문
+	/*
+	for (int i = 0; i < 5; i++)
+	{
+		if (i == 3)
+			continue;
+
+		printf("%d", i);
+	}
+
+	// 1 ~ 10까지 수 출력, 3의 배수 제외
+
+	for (int i = 1; i <= 10; i++)
+	{
+		if (i % 3 == 0)
+			continue;
+
+		printf("%d\n", i);
+	}
+	*/
+
+	// 형 변환
+	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이 이루어질 때
+	// 기존에 지정했던 자료형을 다른 자료형으로 변환하는 과정
+	/*
+	// 암묵적 형 변환
+	// 서로 다른 자료형으로 연산이 이루어질 때
+	// 자료형의 크기가 큰 자료형으로 변환되는 과정
+
+	printf("char의 크기 : %d\n", sizeof(char));
+	printf("short의 크기 : %d\n", sizeof(short));
+	printf("int의 크기 : %d\n", sizeof(int));
+	printf("long의 크기 : %d\n", sizeof(long));
+	printf("float의 크기 : %d\n", sizeof(float));
+
+	char x = 10;
+	short y = 20;
+
+	//  30 = 4 byte (int) + 4 byte (int)
+	int sum = x + y;
+
+	int z = 500;	// [] [] [] []
+	char temp = z;	// []
+
+	printf("temp의 값 : %d\n", temp);
+
+	// 명시적 형 변환
+	int p0 = 10;
+	int p1 = 3;
+
+	float result = (float)p0 / p1;
+	printf("result의 값 : %f\n", result);
+
+	// int와 long의 차이
+	// int : 32비트, 64비트 운영체제에서 4 Byte 크기
+	// long : 32비트 - 4 Byte, 64비트 - 8 Byte
+	// 단, long도 윈도우즈 64비트 운영체제에서 4 Byte
+	*/
+}
--- Source.c
+++ Source.c
@@ -1,102 +1,93 @@
 #include <stdio.h>
 
+// scanf 쓰기 위한 전처리기
+#pragma warning(disable:4996)
+
 void main()
 {
-	// 구구단
+	// scanf 함수
 	/*
-	for (int i = 2; i <= 9; i++)
+	int value = 50;
+
+	for (int i = 0; i < 5; i++)
 	{
-		for (int j = 1; j <= 9; j++)
-		{
-			printf("%d x %d = %d\n", i, j, i * j);
-		}
+		// 버퍼 오버플로 문제 방지 위해 scanf_s 권장
+		scanf_s("%d", &value);
+
+		printf("value의 값 : %d", value);
+	}
+	*/
+
+	// 주소 연산자
+	// 변수의 주소 값을 반환하는 연산자
+	/*
+	int x = 100;
+
+	// 변수의 주소는 프로그램이 실행될 때마다 바뀜
+	// 변수의 주소는 16진수로 표기
+	// 16진수 1 ~ F 까지 표현
+	// 1 2 3 4 5 6 7 8 9 A(10) B(11) C(12) D(13) E(14) F(15)
+	printf("x 변수의 주소 : %p\n", &x);
+	*/
+	// 포인터
+	// 변수의 메모리 주소를 저장하는 변수
+	/*
+	// [] [] [] []
+	int a = 20;
+
+	// ptr은 포인터 변수이며, 변수의 주소 저장
+	// ptr은 a의 주소를 가지고 있으며,
+	// ptr 변수는 a의 시작 주소를 가리킴
+	int * ptr = &a;
+
+	printf("a의 값 : %d\n", a);
+	printf("a의 주소 : %p\n", &a);
+
+	// ptr이 가리키는 값을 출력할 때 & 사용하지 않음
+	printf("ptr이 가리키는 주소 : %p\n", ptr);
+
+	// 역참조
+	
+	*ptr = 100;
+
+	printf("a의 값 : %d\n", a);
+
+	// ptr의 고유 주소를 출력할 때 &를 사용
+	printf("ptr의 고유 주소 : %p\n", &ptr);
+
+	// 포인터의 크기
+	// 32비트 컴파일러에서 4 byte
+	// 64비트 컴파일러에서 8 byte
+
+	printf("포인터 변수의 크기 : %d\n", sizeof(ptr));
+	*/
+
+	// 시프트 연산자
+	// 비트의 값을 주어진 숫자만큼 부호 방향으로 이동시키는 연산자
+	/*
+	// >> 오른쪽으로 비트 이동시키는 부호
+	// << 왼쪽으로 비트 이동시키는 부호
+	
+	// [0] [0] [0] [0] [1] [0] [1] [0] = 10
+	char count = 10;
+	// 왼쪽으로 한 칸 밀었을 때
+	// [0] [0] [0] [1] [0] [1] [0] [0] = 20
+	printf("왼쪽으로 1번 시프트 연산한 결과 : %d\n", count <<= 1);
+
+	printf("count 변수의 값 : %d\n", count);
+
+	// 오른쪽
+	// [0] [0] [0] [0] [0] [0] [1] [0] = 2
+	printf("오른쪽으로 3번 시프트 연산한 결과 : %d\n", count >>= 3);
+	*/
+
+	// ☆ 1 ~ 5 찍기
+	
+	for (int i = 1; i <= 5; i++)
+	{
+		for (int j = 1; j <= i; j++)
+			printf("☆");
 
 		printf("\n");
 	}
-	*/
-
-	// while문
-	// 특정 조건을 만족할 때까지 계속 주어진 명령문 실행하는 반복문
-	/*
-	int memory = 5;
-
-	while (memory > 1)
-	{
-		memory--;
-		printf("조건이 참입니다.\n");
-	}
-	*/
-
-	// do while 문
-	// 조건과 상관없이 한 번의 작업을 수행한 다음 조건에 따라 명령문 실행하는 반복문
-	/*
-	int value = 5;
-
-	do
-	{	// do에서 선언한 변수는 while의 조건으로 사용 X
-		// int x = 10;
-		printf("조건이 거짓입니다.");
-	}
-	while (value == 10);
-	*/
-
-	// continue 문
-	// 해당 조건문만 실행하지 않고, 반복문은 이어서 실행하는 제어문
-	/*
-	for (int i = 0; i < 5; i++)
-	{
-		if (i == 3)
-			continue;
-
-		printf("%d", i);
-	}
-	
-	// 1 ~ 10까지 수 출력, 3의 배수 제외
-
-	for (int i = 1; i <= 10; i++)
-	{
-		if (i % 3 == 0)
-			continue;
-
-		printf("%d\n", i);
-	}
-	*/
-
-	// 형 변환
-	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이 이루어질 때
-	// 기존에 지정했던 자료형을 다른 자료형으로 변환하는 과정
-	/*
-	// 암묵적 형 변환
-	// 서로 다른 자료형으로 연산이 이루어질 때
-	// 자료형의 크기가 큰 자료형으로 변환되는 과정
-	
-	printf("char의 크기 : %d\n", sizeof(char));
-	printf("short의 크기 : %d\n", sizeof(short));
-	printf("int의 크기 : %d\n", sizeof(int));
-	printf("long의 크기 : %d\n", sizeof(long));
-	printf("float의 크기 : %d\n", sizeof(float));
-
-	char x = 10;
-	short y = 20;
-
-	//  30 = 4 byte (int) + 4 byte (int)
-	int sum = x + y;
-
-	int z = 500;	// [] [] [] []
-	char temp = z;	// []
-
-	printf("temp의 값 : %d\n", temp);
-
-	// 명시적 형 변환
-	int p0 = 10;
-	int p1 = 3;
-
-	float result = (float)p0 / p1;
-	printf("result의 값 : %f\n", result);
-
-	// int와 long의 차이
-	// int : 32비트, 64비트 운영체제에서 4 Byte 크기
-	// long : 32비트 - 4 Byte, 64비트 - 8 Byte
-	// 단, long도 윈도우즈 64비트 운영체제에서 4 Byte
-	*/
 }	
