48a948c2f6ac68bacf8f0a4e6075e4b4c18ff438
ksy2949871@naver.com <ksy2949871@naver.com>
2022-08-23 19:28:22 +0900

    2022.08.22(배열의 설명)

--- Source.c
+++ Source.c
@@ -1,93 +1,65 @@
 #include <stdio.h>
 
-// scanf 쓰기 위한 전처리기
-#pragma warning(disable:4996)
-
 void main()
 {
-	// scanf 함수
+	// 포인터의 크기
 	/*
-	int value = 50;
+	double value = 9.36;
+	double x = 10.97;
+	// 포인터 변수를 저장하기 위해 주소 값을 저장할 변수의 자료형과 포인터 변수의 자료형이 일치해야 합니다.
+	double * ptr = &value;
+	printf("ptr의 값 : %lf\n", *ptr);
+	printf("ptr 포인터 변수의 크기 : %d\n", sizeof(ptr));
+	// ptr 변수가 가리키는 주소를 value에서 라는 x라는 변수의 주소로 변경합니다.
+	ptr = &x;
+	printf("ptr의 값 : %lf\n", *ptr);
+	*ptr = 20.52;
+	printf("ptr의 값 : %lf\n", *ptr);
+	*/
 
+	// 배열
+	/*
+	// 같은 자료형의 변수들로 이루어진 유한 집합입니다.
+
+	//   0    1    2    3   4
+	// [10] [20] [30] [40] [50] <- 배열의 요소(인덱스)
+	// 4 byte 메모리 공간으로 5개의 연속적인 메모리 공간을 생성합니다.
+	int array[5];
+	// 배열의 경우 첫 번째 원소는 0부터 시작합니다.
+	array[0] = 10;
+	array[1] = 20;
+	array[2] = 30;
+	array[3] = 40;
+	array[4] = 50;
+	// 배열의 크기를 벗어나서 저장할 수 없습니다.
+	// array[5] = 100;
+	printf("array의 첫 번째 값 : %d\n", array[0]);
+	printf("array의 두 번째 값 : %d\n", array[1]);
+	printf("array의 세 번째 값 : %d\n", array[2]);
+	printf("array의 네 번째 값 : %d\n", array[3]);
+	printf("array의 다섯 번째 값 : %d\n", array[4]);
+	printf("array의 크기 : %d\n", sizeof(array));
+	*/
+
+	// 배열의 초기화
+	/*
+	// 0번째 요소(10) 1번째 요소(5) 2번째 요소(0)
+	int room [3] = { 10,5,0 };
+	// 배열의 크기를 구하는 방법
+	// 배열의 크기 / 배열의 자료형
+	int size = sizeof(room) / sizeof(int);
+	for (int i = 0; i < size; i++)
+	{
+		printf("room %d 번째 요소 : %d\n", i, room[i]);
+	}
+
+	// 배열의 크기를 생략할 수 있으며, 초기화 목록에 설정된   수를 컴파일러가 자동으로 계산해줍니다.
+	double team[ ] = { 5.1, 6.8, 7.7, 49.63, 11.07};
+	printf("team 배열의 크기 : %d\n", sizeof(team));
 	for (int i = 0; i < 5; i++)
 	{
-		// 버퍼 오버플로 문제 방지 위해 scanf_s 권장
-		scanf_s("%d", &value);
-
-		printf("value의 값 : %d", value);
+		team[i] = 0;
+		printf("%d 번째 요소의 값 : %d\n", i, team[i]);
 	}
 	*/
-
-	// 주소 연산자
-	// 변수의 주소 값을 반환하는 연산자
-	/*
-	int x = 100;
-
-	// 변수의 주소는 프로그램이 실행될 때마다 바뀜
-	// 변수의 주소는 16진수로 표기
-	// 16진수 1 ~ F 까지 표현
-	// 1 2 3 4 5 6 7 8 9 A(10) B(11) C(12) D(13) E(14) F(15)
-	printf("x 변수의 주소 : %p\n", &x);
-	*/
-	// 포인터
-	// 변수의 메모리 주소를 저장하는 변수
-	/*
-	// [] [] [] []
-	int a = 20;
-
-	// ptr은 포인터 변수이며, 변수의 주소 저장
-	// ptr은 a의 주소를 가지고 있으며,
-	// ptr 변수는 a의 시작 주소를 가리킴
-	int * ptr = &a;
-
-	printf("a의 값 : %d\n", a);
-	printf("a의 주소 : %p\n", &a);
-
-	// ptr이 가리키는 값을 출력할 때 & 사용하지 않음
-	printf("ptr이 가리키는 주소 : %p\n", ptr);
-
-	// 역참조
-	
-	*ptr = 100;
-
-	printf("a의 값 : %d\n", a);
-
-	// ptr의 고유 주소를 출력할 때 &를 사용
-	printf("ptr의 고유 주소 : %p\n", &ptr);
-
-	// 포인터의 크기
-	// 32비트 컴파일러에서 4 byte
-	// 64비트 컴파일러에서 8 byte
-
-	printf("포인터 변수의 크기 : %d\n", sizeof(ptr));
-	*/
-
-	// 시프트 연산자
-	// 비트의 값을 주어진 숫자만큼 부호 방향으로 이동시키는 연산자
-	/*
-	// >> 오른쪽으로 비트 이동시키는 부호
-	// << 왼쪽으로 비트 이동시키는 부호
-	
-	// [0] [0] [0] [0] [1] [0] [1] [0] = 10
-	char count = 10;
-	// 왼쪽으로 한 칸 밀었을 때
-	// [0] [0] [0] [1] [0] [1] [0] [0] = 20
-	printf("왼쪽으로 1번 시프트 연산한 결과 : %d\n", count <<= 1);
-
-	printf("count 변수의 값 : %d\n", count);
-
-	// 오른쪽
-	// [0] [0] [0] [0] [0] [0] [1] [0] = 2
-	printf("오른쪽으로 3번 시프트 연산한 결과 : %d\n", count >>= 3);
-	*/
-
-	// ☆ 1 ~ 5 찍기
-	
-	for (int i = 1; i <= 5; i++)
-	{
-		for (int j = 1; j <= i; j++)
-			printf("☆");
-
-		printf("\n");
-	}
 }	
